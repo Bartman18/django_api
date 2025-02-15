5745118c338ff72da8d77bdeb898f31c1936bee5
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2025-02-15 12:31:14 +0100

    register new router

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.routers import DefaultRouter
 
@@ -6,21 +6,19 @@ from . import views as v
 
 router = DefaultRouter()
 router.register(r'products', v.ProductViewSet, basename='product')
+router.register(r'reservations', v.ReservationViewSet, basename='reservation')
+router.register(r'orders', v.OrderViewSet, basename='order')
 
 
 urlpatterns = [
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path( 'token/refresh/', v.CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', v.CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', v.RegisterView.as_view(), name='register'),
-    path('login/', v.LoginView.as_view(), name='login'),
-    # path('logout/', v.LogoutView.as_view(), name='logout'),
-    # path('add_product/', v.ProductAddView.as_view(), name='add_product'),
-    #
-    # path('edit_product/', v.ProductUpdateView.as_view(), name='edit_product'),
-    # path('delete_product/', v.ProductDeleteView.as_view(), name='delete_product'),
-    #
-    # path('show_product/', v.ProductListView.as_view(), name='show_product')
+    # path('login/', v.LoginView.as_view(), name='login'),
+    path('user/', v.ProfileView.as_view(), name='user'),
+    path('', include(router.urls)),
+
 
 
 
